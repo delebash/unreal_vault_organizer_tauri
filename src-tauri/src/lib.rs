396de1c41a49c10de79a0699e4a2be1f82fcb67c
delebash <danelebash@hotmail.com>
2024-11-23 15:04:22 -0500

    testing updater

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -3,13 +3,15 @@
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_dialog::init())
+        .plugin(tauri_plugin_updater::Builder::new().build())
         .plugin(tauri_plugin_shell::init())
         .plugin(tauri_plugin_http::init())
         //    .invoke_handler(tauri::generate_handler![http_request])
         .setup(|app| {
             if cfg!(debug_assertions) {
-//                   let window = app.get_webview_window("main").unwrap();
-//                     window.open_devtools();
+                //                   let window = app.get_webview_window("main").unwrap();
+                //                     window.open_devtools();
                 app.handle().plugin(
                     tauri_plugin_log::Builder::default()
                         .level(log::LevelFilter::Info)
