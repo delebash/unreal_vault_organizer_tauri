657b9178ca9bca555ae93bc20ee2daea2aa96ba9
delebash <danelebash@hotmail.com>
2024-11-23 20:15:02 -0500

    check update fixes

--- src/components/CheckForUpdates.vue
+++ src/components/CheckForUpdates.vue
@@ -5,15 +5,15 @@
 <script setup>
 import { check } from '@tauri-apps/plugin-updater';
 import { ask, message } from '@tauri-apps/plugin-dialog';
-import {invoke} from "@tauri-apps/api/core";
+import { relaunch } from '@tauri-apps/plugin-process';
 
 await checkForAppUpdates()
 async function checkForAppUpdates(onUserClick= false) {
   const update = await check();
   if (update === null) {
-    await message('Failed to check for updates.\nPlease try again later.', {
-      title: 'Error',
-      kind: 'error',
+    await message('You are on the latest version.', {
+      title: 'No Update Available',
+      kind: 'info',
       okLabel: 'OK'
     });
   } else if (update?.available) {
@@ -24,18 +24,26 @@ async function checkForAppUpdates(onUserClick= false) {
       cancelLabel: 'Cancel'
     });
     if (yes) {
-      await update.downloadAndInstall();
-      // Restart the app after the update is installed by calling the Tauri command that handles restart for your app
-      // It is good practice to shut down any background processes gracefully before restarting
-      // As an alternative, you could ask the user to restart the app manually
-      await invoke("graceful_restart");
+      let downloaded = 0;
+      let contentLength = 0;
+      await update.downloadAndInstall((event) => {
+        switch (event.event) {
+          case 'Started':
+            contentLength = event.data.contentLength;
+            console.log(`started downloading ${event.data.contentLength} bytes`);
+            break;
+          case 'Progress':
+            downloaded += event.data.chunkLength;
+            console.log(`downloaded ${downloaded} from ${contentLength}`);
+            break;
+          case 'Finished':
+            console.log('download finished');
+            break;
+        }
+      });
+      console.log('update installed');
+      await relaunch();
     }
-  } else if (onUserClick) {
-    await message('You are on the latest version. Stay awesome!', {
-      title: 'No Update Available',
-      kind: 'info',
-      okLabel: 'OK'
-    });
   }
 }
 </script>
